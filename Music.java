62b58e5767a1e6990c239a23fc0cbeea69f96c56
SmexyShimala <jshimala@saintjoe.edu>
2015-02-13 14:22:29 -0600

    Update Music.java

--- Music.java
+++ Music.java
@@ -1,1 +1,23 @@
 public class Music
+
+//Class to keep track of music
+//February 13, 2015
+
+  String album;
+  String artist;
+  String genre;
+  String media;
+  String location;
+  int copies;
+
+/*------------------------*/
+  
+  Music {
+    album = "No Album";
+    artist = "No Artist";
+    genre = "No Genre";
+    media = "No Media";
+    location = "None";
+    copies = 0;
+  }
+  
