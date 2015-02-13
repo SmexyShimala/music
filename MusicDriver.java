f1cd886346e7c5866b04c14b305eb707ea1498b7
SmexyShimala <jshimala@saintjoe.edu>
2015-02-13 14:52:03 -0600

    Update MusicDriver.java

--- MusicDriver.java
+++ MusicDriver.java
@@ -3,6 +3,6 @@ public class MusicDriver {
     Music myMusic = new Music();
     System.out.println(myMusic.toString());
     Music anotherMusic = new Music("Back in Black", "AC/DC", "Metal", "CD");
-    System.out.println(myMusic.toString());
+    System.out.println(anotherMusic.toString());
   }
 }
